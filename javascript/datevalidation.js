8f418d9311531fa7e8151fb638d06491832a62ef
jeffschwartz <jefftschwartz@gmail.com>
2014-11-26 09:33:10 -0500

    Fixed all jshint raised issues in code.

--- javascript/datevalidation.js
+++ javascript/datevalidation.js
@@ -10,28 +10,28 @@
         return typeof(y) === 'number';
     };
     dv.isMonthValid = function isMonthValid(m){
-        if(typeof(m) !== 'number') return false;
+        if(typeof(m) !== 'number') { return false; }
         return m >= 1 && m <= 12;
     };
     dv.isLeapYear = function isYearALeapYear(y){
         //If year is not evenly divisible by 4 it isn't a leap year
-        if(y % 4) return false;
+        if(y % 4) { return false; }
         //If year is evenly divisible by 4 and is not evenly divisible by 100 it is a leap year
-        if(y % 100) return true;
+        if(y % 100) { return true; }
         //If a year is evenly divisible by 4 and it is evenly divisible by 100 and it is evenly divisible by 400 it is a leap year
         return (y % 400 === 0);
     };
     dv.isDayValid = function isDayValid(y, m, d){
         var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         //Validate numerics and min day range
-        if(typeof(d) !== 'number' || d < 1) return false;
+        if(typeof(d) !== 'number' || d < 1) { return false; }
         //Validate for leap year (2/29)
-        if(m === 2 && d === 29) return dv.isLeapYear(y);
+        if(m === 2 && d === 29) { return dv.isLeapYear(y); }
         //Validate for all other dates
-        if(d <= daysInMonths[m - 1] ) return true;
+        if(d <= daysInMonths[m - 1] ) { return true; }
         return false;
     };
     dv.isDateValid = function isDateValid(y, m, d){
-        return (dv.isYearValid(y) && dv.isMonthValid(m) && dv.isDayValid(y, m, d));
+        { return (dv.isYearValid(y) && dv.isMonthValid(m) && dv.isDayValid(y, m, d)); }
     };
 }(window));
